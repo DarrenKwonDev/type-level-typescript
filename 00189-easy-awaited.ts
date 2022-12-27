270723a8b71eaeed82b9833fdc55221a044626e8
DarrenKwonDev <darrenkwondev46@gmail.com>
2022-12-27 10:22:20 +0900

    chore: MyAwaited type callabled then first function args name changed

--- 00189-easy-awaited.ts
+++ 00189-easy-awaited.ts
@@ -15,12 +15,11 @@ type cases = [
     Expect<Equal<MyAwaited<T>, number>>
 ]
 
-// @ts-expect-error
 type error = MyAwaited<number>
 
 // ============= Your Code Here =============
 type MyAwaited<T> = T extends object & {
-    then(onfulfilled: infer F, ...args: infer _): any
+    then(onResolveFunc: infer F, ...args: infer _): any
 }
     ? F extends (value: infer V, ...args: infer _) => any
         ? MyAwaited<V>
@@ -31,9 +30,9 @@ type MyAwaited<T> = T extends object & {
  * Recursively unwraps the "awaited type" of a type. Non-promise "thenables" should resolve to `never`. This emulates the behavior of `await`.
  */
 type Awaited<T> = T extends null | undefined
-    ? T // special case for `null | undefined` when not in `--strictNullChecks` mode
-    : T extends object & { then(onfulfilled: infer F, ...args: infer _): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
-    ? F extends (value: infer V, ...args: infer _) => any // if the argument to `then` is callable, extracts the first argument
+    ? T // strickNullChecks가 활성화되지 않아 null | undefined가 들어온 경우 타입을 그대로 돌려줌
+    : T extends object & { then(onfulfilled: infer F, ...args: infer _): any } // Await 타입은 object이면서 동시에 then을 호출할 수 있어야 함
+    ? F extends (value: infer V, ...args: infer _) => any // resolve 함수가 반환하는 첫번째 값
         ? Awaited<V> // recursively unwrap the value
         : never // the argument to `then` was not callable
-    : T // non-object or non-thenable
+    : T // non-object or non-thenable이면 그대로 반환. 이 타입의 재귀에서 base case가 됨
