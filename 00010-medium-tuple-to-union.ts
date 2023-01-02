8f701847f825d5a1740de8b5a41354eb9f1470e0
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-01-02 15:27:50 +0900

    solved: tuple to union

---
+++ 00010-medium-tuple-to-union.ts
@@ -0,0 +1,10 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+    Expect<Equal<TupleToUnion<[123, "456", true]>, 123 | "456" | true>>,
+    Expect<Equal<TupleToUnion<[123]>, 123>>
+]
+
+// ============= Your Code Here =============
+type TupleToUnion<T extends readonly any[]> = T[number]
