d4f9948f8c8dc326eab0f1cc3c08ba64a141aaac
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-01-05 03:04:23 +0900

    solved: implement startsWith, endsWith in type level

---
+++ 02688-medium-startswith.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+    Expect<Equal<StartsWith<"abc", "ac">, false>>,
+    Expect<Equal<StartsWith<"abc", "ab">, true>>,
+    Expect<Equal<StartsWith<"abc", "abc">, true>>,
+    Expect<Equal<StartsWith<"abc", "abcd">, false>>,
+    Expect<Equal<StartsWith<"abc", "">, true>>,
+    Expect<Equal<StartsWith<"abc", " ">, false>>,
+    Expect<Equal<StartsWith<"", "">, true>>
+]
+
+// ============= Your Code Here =============
+type StartsWith<
+    T extends string,
+    U extends string
+> = T extends `${U}${infer Rest}` ? true : false
---
+++ 02693-medium-endswith.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+    Expect<Equal<EndsWith<"abc", "bc">, true>>,
+    Expect<Equal<EndsWith<"abc", "abc">, true>>,
+    Expect<Equal<EndsWith<"abc", "d">, false>>
+]
+
+// ============= Your Code Here =============
+type EndsWith<
+    T extends string,
+    U extends string
+> = T extends `${infer Rest}${U}` ? true : false
