0840463177a01ce181119d55de30de4a9609b958
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-02-05 03:54:53 +0900

    solved: isunion

---
+++ 01097-medium-isunion.ts
@@ -0,0 +1,27 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+    Expect<Equal<IsUnion<string>, false>>,
+    Expect<Equal<IsUnion<string | number>, true>>,
+    Expect<Equal<IsUnion<"a" | "b" | "c" | "d">, true>>,
+    Expect<Equal<IsUnion<undefined | null | void | "">, true>>,
+    Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+    Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+    Expect<Equal<IsUnion<[string | number]>, false>>,
+    // Cases where T resolves to a non-union type.
+    Expect<Equal<IsUnion<string | never>, false>>,
+    Expect<Equal<IsUnion<string | unknown>, false>>,
+    Expect<Equal<IsUnion<string | any>, false>>,
+    Expect<Equal<IsUnion<string | "a">, false>>,
+    Expect<Equal<IsUnion<never>, false>>
+]
+
+// ============= Your Code Here =============
+type IsUnion<T, K = T> = [T] extends [never]
+    ? false
+    : T extends T
+    ? [K] extends [T]
+        ? false
+        : true
+    : never
